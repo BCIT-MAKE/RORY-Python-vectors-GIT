b3909d56a415b8f81aef9b6e2a2c47191644ce41
Sayed Naseel Mohamed Thangal <mrthangal@hotmail.com>
2026-09-08 12:44:59 -0700

    first commit

---
+++ BallScrewKneeVectors_v1.py
@@ -0,0 +1,13 @@
+#BallScrewKneeVectors_v1
+#Code to compute knee torque when using a ball screw actuator with lever arm. 
+#Vector analysis
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+
+AA=1
+
+print("AA = ", AA)

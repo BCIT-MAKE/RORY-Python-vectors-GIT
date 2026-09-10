36c91828f1a77af23f128d8c864e4268a49d93dd
Sayed Naseel Mohamed Thangal <mrthangal@hotmail.com>
2026-09-10 14:44:01 -0700

    Updated code to sweep angles and plot torque for various angles

--- vectoroperations.py
+++ vectoroperations.py
@@ -9,3 +9,11 @@ def rotate2d(v, angle_deg):
         [np.sin(theta),  np.cos(theta)]
     ])
     return R @ v
+
+# this rotate function can take an anglesweep vector isntead of just a single value.
+def rotate2dsweep(v, angle_deg):
+    theta = np.radians(angle_deg)
+    c, s = np.cos(theta), np.sin(theta)
+    x = v[0]*c - v[1]*s
+    y = v[0]*s + v[1]*c
+    return np.array([x, y])
